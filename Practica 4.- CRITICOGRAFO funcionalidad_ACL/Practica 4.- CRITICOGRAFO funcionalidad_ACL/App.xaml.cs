2b2137b2ceed71dd65a38dfacce5aba4d3d8ec15
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2023-02-11 17:14:23 -0600

    first commit

---
+++ Practica 4.- CRITICOGRAFO funcionalidad_ACL/Practica 4.- CRITICOGRAFO funcionalidad_ACL/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Practica_4.__CRITICOGRAFO_funcionalidad_ACL
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
